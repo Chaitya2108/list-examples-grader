8372030f920a34f6f65ef2f5f1708f19aa742970
Chaitya Jodhavat <chaityajodhavat@gmail.com>
2024-02-20 17:56:02 -0800

    f

--- grade.sh
+++ grade.sh
@@ -1,4 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH2='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar:grading-area'
+
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +9,41 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "Correct file submitted."
+else
+    echo "File not found."
+    exit 1
+fi
+
+cp -r student-submission/ListExamples.java grading-area
+cp -r TestListExamples.java grading-area
+
+if [[ -f grading-area/ListExamples.java ]]
+then 
+    echo "file moved successfully"
+else
+    echo "File not moved"
+    exit 1
+fi
+
+javac -cp $CPATH grading-area/*.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Files compiled successfully."
+    
+else
+    echo "files did not compile"
+    exit 1
+fi
+
+# java -cp $CPATH org.junit.runner.JUnitCore grading-area/TestListExamples
+set +e
+java -cp $CPATH2 org.junit.runner.JUnitCore TestListExamples > grading-area/output.txt
+tail -n 2 grading-area/output.txt
 
 
 # Draw a picture/take notes on the directory structure that's set up after
